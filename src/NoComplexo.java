7487e1401e8821579a8ca6cbb0f54504752922e7
janainafmonteiro <janainaferreiramonteiro@hotmail.com>
2025-12-02 08:58:11 -0300

    Update NoComplexo.java

--- src/NoComplexo.java
+++ src/NoComplexo.java
@@ -2,9 +2,5 @@ public class NoComplexo implements No {
     final NumComplexo v;
     NoComplexo(NumComplexo v){ this.v = v; }
     public NumComplexo eval(){ return v; }
-    static String bonito(double v){
-        if (v == (long)v) return Long.toString((long)v);
-        return Double.toString(v);
-    }
     public String converteLisp(){ return String.format("(complexo %.2f %.2f)", v.getReal(), v.getImaginario()); }
 }
